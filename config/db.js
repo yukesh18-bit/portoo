8c57848962284c8625979b84cb538325ab8111f6
yukesh18-bit <yukeshkumarviiib123@gmail.com>
2026-08-04 16:40:49 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.log("❌ MongoDB Connection Failed");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
